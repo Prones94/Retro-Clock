ebfaf3ce842cbfeb0be3cdf25abb7a5342c01d02
Ian Rones <ronesp.85@gmail.com>
2020-03-29 22:22:03 -0700

    [edit] time is now printed like a clock

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 		"fmt"
+		"time"
 )
 
 /* Step #1 Create Digits */
@@ -86,18 +87,36 @@ func main(){
 		"  █",
 		"███",
 	}
+
+	colon := placeholder{
+		"   ",
+		" ░ ",
+		"   ",
+		" ░ ",
+		"   ",
+	}
 	digits := [...]placeholder {
 		zero, one, two, three, four, five, six, seven, eight, nine,
 	}
 
-	for line := range digits[0] {
-		for digit := range digits {
-			fmt.Print(digits [digit][line], " ")
+	/* Step #2 Print Clock */
+	now := time.Now()
+	hour, min, sec := now.Hour(), now.Minute(), now.Second()
+	
+	fmt.Printf("Hour: %d, Minute: %d, Second: %d\n", hour, min, sec)
+	clock := [...]placeholder { // [8][5] --> [8] placeholder
+		digits[hour/10], digits[hour%10],
+		colon,
+		digits[min/10], digits[min%10],
+		colon,
+		digits[sec/10], digits[sec%10],
+	}
+	for line := range clock[0] {
+		for digit := range clock {
+			fmt.Print(clock [digit][line], " ")
 		}
 		fmt.Println()
 	}
 }
-
-/* Step #2 Print Clock */
 /* Step #3 Animate Clock */
 /* Step #4 Blink Effect */
