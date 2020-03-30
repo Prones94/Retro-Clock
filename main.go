8f73ca3402c051f62069782dfdc1c30d653ca025
Ian Rones <ronesp.85@gmail.com>
2020-03-29 22:57:04 -0700

    [edit] clock colon now counts each second

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-		"fmt"
-		"time"
+	"fmt"
+	"time"
+
+	"github.com/inancgumus/screen"
 )
 
 /* Step #1 Create Digits */
@@ -100,23 +102,33 @@ func main(){
 	}
 
 	/* Step #2 Print Clock */
-	now := time.Now()
-	hour, min, sec := now.Hour(), now.Minute(), now.Second()
-	
-	fmt.Printf("Hour: %d, Minute: %d, Second: %d\n", hour, min, sec)
-	clock := [...]placeholder { // [8][5] --> [8] placeholder
-		digits[hour/10], digits[hour%10],
-		colon,
-		digits[min/10], digits[min%10],
-		colon,
-		digits[sec/10], digits[sec%10],
-	}
-	for line := range clock[0] {
-		for digit := range clock {
-			fmt.Print(clock [digit][line], " ")
+	/* Step #3 Animate Clock */
+	screen.Clear()
+	for { // this creates an infinite loop to always recreate the clock
+		screen.MoveTopLeft()
+		now := time.Now()
+		hour, min, sec := now.Hour(), now.Minute(), now.Second()
+		
+		fmt.Printf("Hour: %d, Minute: %d, Second: %d\n", hour, min, sec)
+		clock := [...]placeholder { // [8][5] --> [8] placeholder
+			digits[hour/10], digits[hour%10],
+			colon, //separator array
+			digits[min/10], digits[min%10],
+			colon, //separator array
+			digits[sec/10], digits[sec%10],
+		}
+		for line := range clock[0] {
+			for index, digit := range clock {
+				next := clock [index][line]
+				if digit == colon && sec%2 == 0{
+					next = "  "
+				}
+				fmt.Print(next, "  ")
+			}
+			fmt.Println()
 		}
 		fmt.Println()
+
+		time.Sleep(time.Second) // function that takes in time.Duration value and stops world for given duration
 	}
 }
-/* Step #3 Animate Clock */
-/* Step #4 Blink Effect */
