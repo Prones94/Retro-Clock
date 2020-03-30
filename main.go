172cb11c3b45e737819f1218b93639b6bd836fd4
Ian Rones <ronesp.85@gmail.com>
2020-03-29 22:01:11 -0700

    [add] placeholder numbers print side by side

--- main.go
+++ main.go
@@ -1,4 +1,103 @@
+package main
+
+import (
+		"fmt"
+)
+
 /* Step #1 Create Digits */
+func main(){
+	type placeholder [5]string
+
+	zero := placeholder{
+		"███",
+		"█ █",
+		"█ █",
+		"█ █",
+		"███",
+	}
+
+	one := placeholder{
+		"██ ",
+		" █ ",
+		" █ ",
+		" █ ",
+		"███",
+	}
+	two := placeholder{
+		"███",
+		"  █",
+		"███",
+		"█  ",
+		"███",
+	}
+
+	three := placeholder{
+		"███",
+		"  █",
+		"███",
+		"  █",
+		"███",
+	}
+
+	four := placeholder{
+		"█ █",
+		"█ █",
+		"███",
+		"  █",
+		"  █",
+	}
+
+	five := placeholder{
+		"███",
+		"█  ",
+		"███",
+		"  █",
+		"███",
+	}
+
+	six := placeholder{
+		"███",
+		"█  ",
+		"███",
+		"█ █",
+		"███",
+	}
+
+	seven := placeholder{
+		"███",
+		"  █",
+		"  █",
+		"  █",
+		"  █",
+	}
+
+	eight := placeholder{
+		"███",
+		"█ █",
+		"███",
+		"█ █",
+		"███",
+	}
+
+	nine := placeholder{
+		"███",
+		"█ █",
+		"███",
+		"  █",
+		"███",
+	}
+	digits := [...]placeholder {
+		zero, one, two, three, four, five, six, seven, eight, nine,
+	}
+
+	for line := range digits[0] {
+		for digit := range digits {
+			fmt.Print(digits [digit][line], " ")
+		}
+		fmt.Println()
+	}
+}
+
 /* Step #2 Print Clock */
 /* Step #3 Animate Clock */
 /* Step #4 Blink Effect */
